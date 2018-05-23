3d5099eff8e70775cab27a94e57f027c345b7ce2
Tyson Williams <tyson.j.williams@gmail.com>
2018-05-22 18:07:44 -0600

    Create Bookmarks class

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Create models here
+class Bookmarks(models.Model):
+  id = models.UUIDField(primary_key=True, defualt=uuid4, editable=False)
+  url = models.URLField('URL', unique=True)
+  name = models.CharField(max_length=200)
+  notes = models.TextField(blank=True)
