1e7dd5e11adc6e7100b5a378fec0b5c7b3fdea2d
Tyson Williams <tyson.j.williams@gmail.com>
2018-05-22 18:21:17 -0600

    import uuid and fix typo

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from uuid import uuid4
 
 # Create models here
 class Bookmark(models.Model):
-  id = models.UUIDField(primary_key=True, defualt=uuid4, editable=False)
+  id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   url = models.URLField('URL', unique=True)
   name = models.CharField(max_length=200)
   notes = models.TextField(blank=True)
