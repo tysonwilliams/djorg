e0f9f12b0be46295723d3ebdbed2ee2200e02648
Tyson Williams <tyson.j.williams@gmail.com>
2018-05-25 10:24:25 -0600

    Create a personal bookmark

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create models here
+
 class Bookmark(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   url = models.URLField('URL', unique=True)
@@ -9,4 +11,6 @@ class Bookmark(models.Model):
   notes = models.TextField(blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
-  
+
+class PersonalBookmark(Bookmark):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
