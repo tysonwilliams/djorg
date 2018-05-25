a3aeae48c306e9130e3b6f63d5f41335e38e3686
Tyson Williams <tyson.j.williams@gmail.com>
2018-05-25 10:36:13 -0600

    Update personal bookmarks to show only to logged in user

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
 
 # Create your views here
 
 def index(request):
   context = {
     "bookmarks": Bookmark.objects.all(),
+    "personal_bookmarks": PersonalBookmark.objects.filter(user=request.user),
   }
 
   return render(request, "bookmarks/index.html", context)
