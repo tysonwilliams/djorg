fa8c67a7880bd929b2bfb70a7f87c1f4643ae121
Tyson Williams <tyson.j.williams@gmail.com>
2018-05-25 11:01:40 -0600

    Filter out personal bookmarks

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -4,9 +4,11 @@ from .models import Bookmark, PersonalBookmark
 # Create your views here
 
 def index(request):
-  context = {
-    "bookmarks": Bookmark.objects.all(),
-  }
+  context = {}
+
+  pbid = PersonalBookmark.objects.values_list("id")
+
+  context["bookmarks"] = Bookmark.objects.exclude(id__in=pbid)
 
   if request.user.is_anonymous:
     context["personal_bookmarks"] = PersonalBookmark.objects.none()
