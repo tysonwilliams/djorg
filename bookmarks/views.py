8541be310b2e24c239a2be8c2de99c4341a7dbcd
Tyson Williams <tyson.j.williams@gmail.com>
2018-05-25 11:08:32 -0600

    Add form to view

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Bookmark, PersonalBookmark
+from .forms import BookmarkForm
 
 # Create your views here
 
@@ -14,4 +15,7 @@ def index(request):
     context["personal_bookmarks"] = PersonalBookmark.objects.none()
   else:
     context["personal_bookmarks"] = PersonalBookmark.objects.filter(user=request.user)
+  
+  context["form"] = BookmarkForm
+  
   return render(request, "bookmarks/index.html", context)
