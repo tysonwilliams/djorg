716bb3a8b8e4a06d13bbaa113ea3ba394e9e7a9e
Tyson Williams <tyson.j.williams@gmail.com>
2018-05-29 17:19:39 -0600

    Add rest framework to settings.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -125,3 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django REST framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
