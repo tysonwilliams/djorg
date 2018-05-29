3f250bb50873cc02b88e3496013b4af9a6c1271b
Tyson Williams <tyson.j.williams@gmail.com>
2018-05-29 17:16:00 -0600

    Add notes app Create notes app Add 'notes' to installed apps in settings.py Install and add rest_framework

--- djorg/settings.py
+++ djorg/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bookmarks',
+    'notes',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
