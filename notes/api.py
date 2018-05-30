6ac099180e9888f20308d453e80db6c4a8f4c96d
Tyson Williams <tyson.j.williams@gmail.com>
2018-05-29 19:23:08 -0600

    Write create function for creating new notes

--- notes/api.py
+++ notes/api.py
@@ -3,6 +3,13 @@ from .models import Note
 
 class NoteSerializer(serializers.HyperlinkedModelSerializer):
 
+  def create(self, validated_data):
+    # import pdb; pdb.set_trace()
+    user = self.context['request'].user
+
+    note = Note.objects.create(user=user, **validated_data)
+    return note
+
   class Meta:
     model = Note
     fields = ('title', 'content')
