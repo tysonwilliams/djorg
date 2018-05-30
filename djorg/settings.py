46261111d93ce687f8a72153b506485d92f97276
Tyson Williams <tyson.j.williams@gmail.com>
2018-05-29 18:30:21 -0600

    Update database config

--- djorg/settings.py
+++ djorg/settings.py
@@ -79,13 +79,15 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-DATABASES["default"] = dj_database_url.parse("sqlite:///db.sqlite3", conn_max_age=600)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {}
+DATABASES["default"] = dj_database_url.config(default=config('DATABASE_URL'))
 
 
 # Password validation
